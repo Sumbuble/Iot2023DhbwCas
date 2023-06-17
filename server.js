5efe802ada7bab1e380e42272b4a961a717be82d
Julian <Kuglerjulian@web.de>
2023-06-17 09:32:11 +0200

    re-org

--- server.js
+++ server.js
@@ -4,10 +4,21 @@ var server = http.createServer(function (req, res) {
    
     if (req.url == '/restmuell/collection') { //check the URL of the current request
             res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.write(JSON.stringify({ nextDate: new Date("2015-03-25"), dayUntil: 2}));  
+            res.write(JSON.stringify({ nextDate: new Date("2015-03-25"), dayUntil: 2}));
+            
+            let msg = getNextRestmuellTag();
+            //res.write(JSON.stringify(msg));
+            //TODO: .ics einlesen, nächste Restmüll Abholung rauslesen und Differenz zwischen jetzt und dem Tag berechnen  
             res.end();  
     }
 });
+
+function getNextRestmuellTag()
+{
+    var json;
+    //TODO .ics parsen (https://github.com/jens-maus/node-ical)
+    return json;
+}
 
 server.listen(5000);
 
